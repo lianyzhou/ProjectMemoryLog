a7e93142f3a50b17d0b9087463d648f01fe43937
lianyzhou <lianyzhou@gmail.com>
2014-10-08 09:42:29 +0800

    1.修复日志路径错误。 2.添加最大值、最小值统计。

--- service/log.js
+++ service/log.js
@@ -4,8 +4,8 @@ var fs = require("fs");
 var _ = require("underscore");
 var moment = require('moment');
 
-function getFile() {
-	var fileName = moment().format('YYYY-MM-DD');
+function getFile(date) {
+	var fileName = date || moment().format('YYYY-MM-DD');
 	var filePath = path.resolve(__dirname , '../logs', fileName + ".log");
 	return filePath;
 }
@@ -54,21 +54,31 @@ function buildChart(arr) {
 	var endTime = arr[arr.length - 1].time;
 	var times = [],
 		memory = [];
+	var max = 0 , min = Number.MAX_VALUE;
 	for(var i = 0 , len = arr.length ; i < len ; i++) {
 		var item = arr[i];
+		var val = parseFloat((item.memory / 1000).toFixed(2));
 		times.push(moment(item.time).format('HH:mm:ss'));
-		memory.push(parseFloat((item.memory / 1000).toFixed(2)));
+		memory.push(val);
+		if(val > max) {
+			max = val;
+		}
+		if(val < min) {
+			min = val;
+		}
 	}
 	return {
 		startTime : startTime,
 		endTime : endTime,
 		times : times,
-		memory : memory
+		memory : memory,
+		max : max,
+		min : min
 	};
 }
 
-exports.getLogCharts = function() {
-	var filePath = getFile();
+exports.getLogCharts = function(date) {
+	var filePath = getFile(date);
 	if(!fs.existsSync(filePath)) {
 		return [];
 	}
